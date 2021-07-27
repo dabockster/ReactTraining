a79c9c88a0b5f7b182785b2810cead7dc1d7203d
Steven Bock <steven@stevenbock.me>
2021-07-27 10:55:43 -0700

    Pulling data from Github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// https://api.github.com/users/dabockster
+
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if(!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if (loading)
+    return <h1>Loading...</h1>;
+  if (error)
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data)
+    return null;
+
+  if(data){
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.location}</p>
+        <img alt={data.login} src={data.avatar_url}/>
+      </div>
+    );
+  }
+
+  return <div>No User Available</div>;
 }
 
 export default App;
