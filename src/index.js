2df6186775bb4afeb9d43a4d16b249973b0fb699
Steven Bock <steven@stevenbock.me>
2021-07-27 13:36:17 -0700

    Created routes in React Router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from "react-router-dom";
 
 
 ReactDOM.render(
-  <App login="dabockster"/>,
+  <Router>
+    <App login="dabockster"/>
+  </Router>,
   document.getElementById('root')
 );
